a96def04682c7612535228a5b02f8decbee7ccd3
ECH2O <1138493417@qq.com>
2023-07-18 02:27:05 +0000

    add some endpoint

--- kernel_program/main.py
+++ kernel_program/main.py
@@ -12,13 +12,18 @@ import threading
 import time
 from queue import Queue
 
+from collections import deque
 from dotenv import load_dotenv
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_from_directory
 from flask_cors import CORS  # Import the CORS library
 
-load_dotenv('.env')
+load_dotenv(".env")
 
-
+OPENAI_API_TYPE = os.environ.get("OPENAI_API_TYPE", "openai")
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY", "")
+OPENAI_BASE_URL = os.environ.get("OPENAI_BASE_URL", "https://api.openai.com")
+OPENAI_API_VERSION = os.environ.get("OPENAI_API_VERSION", "2023-03-15-preview")
+AZURE_OPENAI_DEPLOYMENT = os.environ.get("AZURE_OPENAI_DEPLOYMENT", "")
 APP_PORT = int(os.environ.get("API_PORT", 443))
 
 # Get global logger
@@ -34,10 +39,10 @@ send_queue = Queue()
 messaging = None
 
 # We know this Flask app is for local use. So we can disable the verbose Werkzeug logger
-log = logging.getLogger('werkzeug')
+log = logging.getLogger("werkzeug")
 log.setLevel(logging.ERROR)
 
-cli = sys.modules['flask.cli']
+cli = sys.modules["flask.cli"]
 cli.show_server_banner = lambda *x: None
 
 app = Flask(__name__)
@@ -57,8 +62,7 @@ def start_kernel_manager():
     # Write PID as <pid>.pid to config.KERNEL_PID_DIR
     os.makedirs(config.KERNEL_PID_DIR, exist_ok=True)
     with open(
-        os.path.join(config.KERNEL_PID_DIR, "%d.pid" %
-                     kernel_manager_process.pid), "w"
+        os.path.join(config.KERNEL_PID_DIR, "%d.pid" % kernel_manager_process.pid), "w"
     ) as p:
         p.write("kernel_manager")
 
@@ -78,15 +82,13 @@ async def start_snakemq():
         if message["type"] == "status":
             if message["value"] == "ready":
                 logger.debug("Kernel is ready.")
-                result_queue.put(
-                    {"value": "Kernel is ready.", "type": "message"})
+                result_queue.put({"value": "Kernel is ready.", "type": "message"})
 
         elif message["type"] in ["message", "message_raw", "image/png", "image/jpeg"]:
             # TODO: 1:1 kernel <> channel mapping
             logger.debug("%s of type %s" % (message["value"], message["type"]))
 
-            result_queue.put(
-                {"value": message["value"], "type": message["type"]})
+            result_queue.put({"value": message["value"], "type": message["type"]})
 
     messaging.on_message_recv.add(on_recv)
     logger.info("Starting snakemq loop")
@@ -114,7 +116,7 @@ async def start_snakemq():
     await asyncio.gather(async_send_queued_messages(), async_link_loop())
 
 
-@app.route("/api", methods=["POST", "GET"])
+@app.route("/python", methods=["POST", "GET"])
 def handle_request():
     if request.method == "GET":
         # Handle GET requests by sending everything that's in the receive_queue
@@ -126,6 +128,56 @@ def handle_request():
         send_queue.put(data)
 
         return jsonify({"result": "success"})
+
+
+@app.route("/generate", methods=["POST"])
+def generate_code():
+    user_prompt = request.json.get("prompt", "")
+    user_openai_key = request.json.get("openAIKey", None)
+    model = request.json.get("model", None)
+
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
+    code, status = loop.run_until_complete(
+        get_code(user_prompt, user_openai_key, model)
+    )
+    loop.close()
+
+    # Append all messages to the message buffer for later use
+    message_buffer.append(user_prompt + "\n\n")
+
+    return jsonify({"code": code}), status
+
+
+@app.route("/upload", methods=["POST"])
+def upload_file():
+    # check if the post request has the file part
+    if "file" not in request.files:
+        return jsonify({"error": "No file part in the request"}), 400
+    file = request.files["file"]
+    # if user does not select file, browser also
+    # submit an empty part without filename
+    if file.filename == "":
+        return jsonify({"error": "No selected file"}), 400
+    if file:
+        file.save(os.path.join(app.config["UPLOAD_FOLDER"], file.filename))
+        return jsonify({"result": "File successfully uploaded"}), 200
+    else:
+        return jsonify({"error": "File type not allowed"}), 400
+
+
+@app.route("/download")
+def download_file():
+    # Get query argument file
+    file = request.args.get("file")
+    if file is None:
+        return jsonify({"error": "file must not be null"}), 400
+    # from `workspace/` send the file
+    # make sure to set required headers to make it download the file
+    return send_from_directory(
+        os.path.join(os.getcwd(), "workspace"), file, as_attachment=True
+    )
 
 
 @app.route("/restart", methods=["POST"])
@@ -140,6 +192,121 @@ def handle_restart():
 def handle_stop():
     cleanup_kernel_program()
     return jsonify({"result": "bye~"})
+
+
+class LimitedLengthString:
+    def __init__(self, maxlen=2000):
+        self.data = deque()
+        self.len = 0
+        self.maxlen = maxlen
+
+    def append(self, string):
+        self.data.append(string)
+        self.len += len(string)
+        while self.len > self.maxlen:
+            popped = self.data.popleft()
+            self.len -= len(popped)
+
+    def get_string(self):
+        result = "".join(self.data)
+        return result[-self.maxlen :]
+
+
+message_buffer = LimitedLengthString()
+
+
+async def get_code(user_prompt, user_openai_key=None, model="gpt-3.5-turbo"):
+    prompt = f"""First, here is a history of what I asked you to do earlier. 
+    The actual prompt follows after ENDOFHISTORY. 
+    History:
+    {message_buffer.get_string()}
+    ENDOFHISTORY.
+    Write Python code, in a triple backtick Markdown code block, that does the following:
+    {user_prompt}
+    
+    Notes: 
+        First, think step by step what you want to do and write it down in English.
+        Then generate valid Python code in a code block 
+        Make sure all code is valid - it be run in a Jupyter Python 3 kernel environment. 
+        Define every variable before you use it.
+        For data munging, you can use 
+            'numpy', # numpy==1.24.3
+            'dateparser' #dateparser==1.1.8
+            'pandas', # matplotlib==1.5.3
+            'geopandas' # geopandas==0.13.2
+        For pdf extraction, you can use
+            'PyPDF2', # PyPDF2==3.0.1
+            'pdfminer', # pdfminer==20191125
+            'pdfplumber', # pdfplumber==0.9.0
+        For data visualization, you can use
+            'matplotlib', # matplotlib==3.7.1
+        Be sure to generate charts with matplotlib. If you need geographical charts, use geopandas with the geopandas.datasets module.
+        If the user has just uploaded a file, focus on the file that was most recently uploaded (and optionally all previously uploaded files)
+    
+    Teacher mode: if the code modifies or produces a file, end your output AFTER YOUR CODE BLOCK with a link to it as <a href='/download?file=INSERT_FILENAME_HERE'>Download file</a>. Replace INSERT_FILENAME_HERE with the actual filename. So just print that HTML to stdout at the end, AFTER your code block."""
+    temperature = 0.7
+    message_array = [
+        {
+            "role": "user",
+            "content": prompt,
+        },
+    ]
+
+    final_openai_key = OPENAI_API_KEY
+    if user_openai_key:
+        final_openai_key = user_openai_key
+
+    if OPENAI_API_TYPE == "openai":
+        data = {
+            "model": model,
+            "messages": message_array,
+            "temperature": temperature,
+        }
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {final_openai_key}",
+        }
+
+        response = requests.post(
+            f"{OPENAI_BASE_URL}/v1/chat/completions",
+            data=json.dumps(data),
+            headers=headers,
+        )
+    elif OPENAI_API_TYPE == "azure":
+        data = {
+            "messages": message_array,
+            "temperature": temperature,
+        }
+        headers = {
+            "Content-Type": "application/json",
+            "api-key": f"{final_openai_key}",
+        }
+
+        response = requests.post(
+            f"{OPENAI_BASE_URL}/openai/deployments/{AZURE_OPENAI_DEPLOYMENT}/chat/completions?api-version={OPENAI_API_VERSION}",
+            data=json.dumps(data),
+            headers=headers,
+        )
+    else:
+        return "Error: Invalid OPENAI_PROVIDER", 500
+
+    def extract_code(text):
+        # Match triple backtick blocks first
+        triple_match = re.search(r"```(?:\w+\n)?(.+?)```", text, re.DOTALL)
+        if triple_match:
+            return triple_match.group(1).strip()
+        else:
+            # If no triple backtick blocks, match single backtick blocks
+            single_match = re.search(r"`(.+?)`", text, re.DOTALL)
+            if single_match:
+                return single_match.group(1).strip()
+        # If no code blocks found, return original text
+        return text
+
+    if response.status_code != 200:
+        return "Error: " + response.text, 500
+
+    return extract_code(response.json()["choices"][0]["message"]["content"]), 200
 
 
 async def main():
@@ -159,4 +326,3 @@ def run_flask_app():
 
 if __name__ == "__main__":
     asyncio.run(main())
-
